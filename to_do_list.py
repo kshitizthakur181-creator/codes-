20bf0c8e93be99c39ef942a593530fd62d4ba4b5
Kshitiz Thakur <kshitizthakur181@gmail.com>
2026-04-25 21:50:30 +0530

    load task program

--- to_do_list.py
+++ to_do_list.py
@@ -1,16 +1,24 @@
 import os 
 import json
 
-
+file_name = "todo_list.json"
 
 # Simple Features 
 
 
+def load_task():
+    
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file :
+            return json.load(file)
+    
+    else :
+        print("No such file exsist")
+
 def add_task() :
     pass
 
-def load_task():
-    pass
+
 
 
 def completed_task():
@@ -18,3 +26,10 @@ def completed_task():
 
 def edit_task():
     pass
+
+
+# Working 
+
+
+
+
