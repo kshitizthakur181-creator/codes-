3e9d37cb550b00e7ae9d7d3ae659e38367c61c37
Kshitiz Thakur <kshitizthakur181@gmail.com>
2026-04-25 21:53:02 +0530

    adding task program

--- to_do_list.py
+++ to_do_list.py
@@ -1,35 +1,33 @@
-import os 
+import os
 import json
 
 file_name = "todo_list.json"
 
-# Simple Features 
+# Simple Features
 
 
 def load_task():
-    
+
     if os.path.exists(file_name):
-        with open(file_name, "r") as file :
+        with open(file_name, "r") as file:
             return json.load(file)
-    
-    else :
+
+    else:
         print("No such file exsist")
 
-def add_task() :
-    pass
 
-
+def add_task(data):
+    with open(file_name, "w") as file:
+        json.dump(data, file, indent=4)
+    print("Task Saved")
 
 
 def completed_task():
     pass
 
+
 def edit_task():
     pass
 
 
-# Working 
-
-
-
-
+# Working
